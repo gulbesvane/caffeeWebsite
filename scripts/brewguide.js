4ee0ca1c3483a8ea54adffa34c73d29baa38ef01
Paulina Stachera Diaz <stachera.p@gmail.com>
2021-12-08 15:12:30 +0000

    Trying JS solutions and adjusting some styling on products page

---
+++ scripts/brewguide.js
@@ -0,0 +1,24 @@
+function espresso(){
+    var targetEspresso=document.getElementById('espresso')
+    document.getElementById("guide").innerHTML=targetEspresso
+}
+function v60(){
+    var targetv60=document.getElementById('v60')
+    document.getElementById("guide").innerHTML=targetv60
+}
+function aeropress(){
+    var targetAeropress=document.getElementById('aeropress')
+    document.getElementById("guide").innerHTML=targetAeropress
+}
+function frenchpress(){
+    var targetFrenchpress=document.getElementById('frenchpress')
+    document.getElementById("guide").innerHTML=targetFrenchpress
+}
+function chemex(){
+    var targetChemex=document.getElementById('chemex')
+    document.getElementById("guide").innerHTML=targetChemex
+}
+function mokapot(){
+    var targetMokapot=document.getElementById('mokapot')
+    document.getElementById("guide").innerHTML=targetMokapot
+}
