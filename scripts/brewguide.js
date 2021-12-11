4caad2785d805a6be3002cb0eab580dc6bff49b7
Paulina Stachera Diaz <stachera.p@gmail.com>
2021-12-11 00:43:01 +0000

    Added jQuery on the product/brew guides page

--- scripts/brewguide.js
+++ scripts/brewguide.js
@@ -1,26 +1,55 @@
-/*function espresso(){
-    var targetEspresso=document.getElementById('espresso')
-    document.getElementById("guide").innerHTML=targetEspresso
-}
-function v60(){
-    var targetv60=document.getElementById('v60')
-    document.getElementById("guide").innerHTML=targetv60
-}
-function aeropress(){
-    var targetAeropress=document.getElementById('aeropress')
-    document.getElementById("guide").innerHTML=targetAeropress
-}
-function frenchpress(){
-    var targetFrenchpress=document.getElementById('frenchpress')
-    document.getElementById("guide").innerHTML=targetFrenchpress
-}
-function chemex(){
-    var targetChemex=document.getElementById('chemex')
-    document.getElementById("guide").innerHTML=targetChemex
-}
-function mokapot(){
-    var targetMokapot=document.getElementById('mokapot')
-    document.getElementById("guide").innerHTML=targetMokapot
-}
+$(document).ready(function(){
+    $("#espresso").click(function(){
+        $("#espressoguide").show();
+        $("#v60guide").hide();
+        $("#aeropressguide").hide();
+        $("#frenchpressguide").hide();
+        $("#chemexguide").hide();
+        $("#mokapotguide").hide();
+    });
 
-$('#V60').html($('#V60').html());*/
+    $("#v60").click(function(){
+        $("#espressoguide").hide();
+        $("#v60guide").show();
+        $("#aeropressguide").hide();
+        $("#frenchpressguide").hide();
+        $("#chemexguide").hide();
+        $("#mokapotguide").hide();
+    });
+  
+    $("#aeropress").click(function(){
+        $("#espressoguide").hide();
+        $("#v60guide").hide();
+        $("#aeropressguide").show();
+        $("#frenchpressguide").hide();
+        $("#chemexguide").hide();
+        $("#mokapotguide").hide();
+    });
+  
+    $("#frenchpress").click(function(){
+        $("#espressoguide").hide();
+        $("#v60guide").hide();
+        $("#aeropressguide").hide();
+        $("#frenchpressguide").show();
+        $("#chemexguide").hide();
+        $("#mokapotguide").hide();
+    });
+
+    $("#chemex").click(function(){
+        $("#espressoguide").hide();
+        $("#v60guide").hide();
+        $("#aeropressguide").hide();
+        $("#frenchpressguide").hide();
+        $("#chemexguide").show();
+        $("#mokapotguide").hide();
+    });
+
+    $("#mokapot").click(function(){
+        $("#espressoguide").hide();
+        $("#v60guide").hide();
+        $("#aeropressguide").hide();
+        $("#frenchpressguide").hide();
+        $("#chemexguide").hide();
+        $("#mokapotguide").show();
+    });
+  });
